5e0552a7f92c4351c9bf836377ece513c55c84d5
Lily <lilydanhaive@gmail.com>
2018-10-10 16:26:30 +0200

    YEAH its Done

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-let essai = 0;
+let essai = 1;
 let min = 20;
 let max = 80;
 
+function compareNb() {
 
-let chooseNb = prompt("choisi un nombre entre 20 et 80");
-let theNumber = parseInt(chooseNb);
-    
-function compareNb(x) {
+    let findNumber = false;
 
-    if (x < randomNb) {
-        alert("le nombre voulu est plus élevé");
-        essai++;
-        compareNb();
+    while (essai < 6 && findNumber == false) {
+        let theNumber = prompt("choisi un nombre entre 20 et 80");
+        if (theNumber < randomNb) {
+            alert("le nombre voulu est plus élevé");
+            essai++;
 
-    } else if (x > randomNb) {
-        alert("le nombre voulu est moins élevé");
-        essai++;
-        compareNb();
+        } else if (theNumber > randomNb) {
+            alert("le nombre voulu est moins élevé");
+            essai++;
 
-    } else {
-        alert("c'est le bon nombre");
+        } else if (theNumber == randomNb) {
+            findNumber = true;
+            confirm("Vous avez trouvé le Juste prix en " + essai + " essais!");
+        } else {
+            alert("Rentrez un nombre correct");
+        }
+    };
+    if (essai >= 6) {
+        alert("vous avez perdu !");
     }
 };
-let nombreAlea = function() {
-    let randomNb = Math.round((Math.random() * (max - min)) + min);
 
-    console.log(randomNb);
+let randomNb = Math.round((Math.random() * (max - min)) + min);
 
-    compareNb()
-};
+console.log(randomNb);
 
-nombreAlea();
+compareNb()
